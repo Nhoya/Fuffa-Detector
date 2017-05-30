1035eeee7aa6252aca9589a309dc81a65597f405
Nhoya <nhoyadx@gmail.com>
2017-05-30 14:20:31 +0200

    added UserAgent to request

--- FuffaDetector.py
+++ FuffaDetector.py
@@ -9,13 +9,14 @@ except IndexError:
     print("Usage:\n FuffaDetector.py [url]")
     exit(2)
 try:
-    html = urllib.request.urlopen(url).read()
+    req = urllib.request.Request(url,data=None,headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0'})
+    html = urllib.request.urlopen(req).read()
 except ValueError:
     print("Invalid Url")
     exit(2)
 
 soup = BeautifulSoup(html, "lxml")
-for script in soup(["script","ul", "li", "a","style","meta","br","h1","h2","h3","h4","h5","h6","span"]):
+for script in soup(["script","ul", "li", "a","style","meta","br","h1","h2","h3","h4","h5","h6"]):
     script.extract()
 text = soup.get_text()
 lines = (line.strip() for line in text.splitlines())
@@ -29,10 +30,10 @@ matches = re.finditer(regex,text,re.IGNORECASE)
 for cyberNum, cyber in enumerate(matches):
     cyberNum = cyberNum + 1
 
-if cyberNum == 0:
-    print("Probably No fuffa inside")
-elif cyberNum <= 3:
-    print("Fuffa inside")
-elif cyberNum >= 4:
-    print("High quantity of fuffa")
-
+try:
+    if cyberNum <= 3:
+        print("Fuffa inside")
+    elif cyberNum >= 4:
+        print("High quantity of fuffa")
+except NameError:
+    print("Probably no fuffa inside")
